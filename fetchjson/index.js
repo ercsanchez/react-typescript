bbc6369db38b61617768d20b49b2382234a16bac
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-17 04:33:28 +0800

    26-323 deliberately code some errors

--- fetchjson/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-var url = "http://jsonplaceholder.typicode.com/todos/1";
-var response = axios_1["default"].get(url);
-response.then(function (res) {
-    console.log(res.data);
-});
--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -4,5 +4,15 @@ const url = "http://jsonplaceholder.typicode.com/todos/1";
 
 const response = axios.get(url);
 response.then((res) => {
-  console.log(res.data);
+  const todo = res.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+  The Todo with ID: ${ID}
+  Has a title of: ${title}
+  Is it finished? ${finished}
+  `);
 });
