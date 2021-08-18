26534a5d08cdd839720f4bf89b7ba1e703061b95
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-19 01:15:18 +0800

    26-362 class method modifiers

--- ts-features/classes.ts
+++ ts-features/classes.ts
@@ -1,23 +1,29 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  protected honk(): void {
     console.log("beep");
   }
 }
 
+// const vehicle = new Vehicle();
+// vehicle.honk();
+
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     // overriding methods in child class
     console.log("vroom");
   }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
 
 const car = new Car();
-car.drive();
-car.honk();
+// car.drive();
+car.startDrivingProcess();
+// car.honk();
