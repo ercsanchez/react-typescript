3d86a46c3b9c424fb1a4af5734f5818d1582b826
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-19 01:25:21 +0800

    26-363 fields in classes | class field modifiers

--- ts-features/classes.ts
+++ ts-features/classes.ts
@@ -1,29 +1,30 @@
 class Vehicle {
+  // color: string = "red";  // either initialize here or inside of constructor | pick one
+  // color: string;
+
+  // shortcut
+  constructor(public color: string) {
+    // this.color = color;
+  }
   protected honk(): void {
     console.log("beep");
   }
 }
 
-// const vehicle = new Vehicle();
-// vehicle.honk();
+const vehicle = new Vehicle("red");
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    // overriding methods in child class
-    console.log("vroom");
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     // overriding methods in child class
+//     console.log("vroom");
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
-
-const car = new Car();
-// car.drive();
-car.startDrivingProcess();
-// car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
