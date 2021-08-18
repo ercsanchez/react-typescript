4edaad7c592e2218f1b90a73a5e4ff5b85676b7b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-18 18:00:55 +0800

    26-333 annotations with variables | arrays | classes | object literals

--- ts-features/annotations.ts
+++ ts-features/annotations.ts
@@ -12,3 +12,18 @@ let nothing: undefined = undefined;
 
 // built-in objects
 let now: Date = new Date();
+
+// array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literals
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
