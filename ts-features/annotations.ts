04ec1f98edbc195090f40ecc2346eb0459e1df0c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-18 17:49:20 +0800

    26-332 annotations with variables | primitives

---
+++ ts-features/annotations.ts
@@ -0,0 +1,14 @@
+// type annotations
+
+// using annotations with variable assignments
+
+// primitives
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built-in objects
+let now: Date = new Date();
