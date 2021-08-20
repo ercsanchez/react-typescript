ace335788c45f4507c98fc33bc847859d2d86a1f
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-21 06:02:34 +0800

    3-19 applying types to refs

---
+++ test-app/src/refs/UserSearch.tsx
@@ -0,0 +1,34 @@
+import { useState, useRef } from "react";
+
+const users = [
+  { name: "Sarah", age: 20 },
+  { name: "Alex", age: 20 },
+  { name: "Michael", age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onClick = () => {
+    const foundUser = users.find((user) => user.name === name);
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      <h3>User Search</h3>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>{user && `${user.name} ${user.age}`}</div>
+    </div>
+  );
+};
+
+export default UserSearch;
